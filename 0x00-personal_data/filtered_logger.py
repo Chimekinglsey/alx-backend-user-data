595aac95d72cfee843cf45fb5b2d92babf6bbe19
chimekinglsey <onlinekingsley@gmail.com>
2023-11-03 01:52:59 +0100

    format method with filter_datum()

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,28 @@
 #!/usr/bin/env python3
 """0 - REGEX-ING Module"""
 import re
-from typing import List
+from typing import List, Tuple
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+    # logging.Formatter(fmt=FORMAT)
+
+    def __init__(self, fields: Tuple) -> None:
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """filter values in incoming log records using filter_datum() """
+
+        return filter_datum(self.fields, self.REDACTION,
+                            record.msg, self.SEPARATOR)
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,

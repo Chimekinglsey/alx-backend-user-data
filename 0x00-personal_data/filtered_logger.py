c9631046af670f10b9ef7ecce822f6d9ba6bd841
chimekinglsey <onlinekingsley@gmail.com>
2023-11-03 01:06:04 +0100

    debugging

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,10 +4,10 @@ import re
 from typing import List
 
 
-def filter_datum(field: List[str], redaction: str, message: str,
+def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-    """Obfuscates private fields in message """
-    for item in field:
-        message = re.sub(rf'{item}.[^\{separator}]+',
-                         f'{item}={redaction}', message)
+    """ Basic login function """
+    for specifics in fields:
+        message = re.sub(rf"{specifics}=(.*?)\{separator}",
+                         f'{specifics}={redaction}{separator}', message)
     return message

e5b59b6273492ff67c5803da6b98fe9451c32dc6
chimekinglsey <onlinekingsley@gmail.com>
2023-11-07 00:42:08 +0100

    route authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,17 +10,16 @@ class Auth:
         pass
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Returns True for paths that need auths, False for excluded paths"""
-        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+        """Define which routes don't need authentication"""
+        if path is None or excluded_paths is None or len(excluded_paths) < 1:
             return True
         if not path.endswith('/'):
             path = path + '/'
         for route in excluded_paths:
             if not route.endswith('/'):
                 route = route + '/'
-        if path in excluded_paths:
-            return False
-
+        return False if path in excluded_paths else True
+    
     def authorization_header(self, request=None) -> str:
         """Returns None, will implement http request header"""
         return None

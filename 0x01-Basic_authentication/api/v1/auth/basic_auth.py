e60424801d0b44e75f4f7c9fe41afc3b2eaff13d
chimekinglsey <onlinekingsley@gmail.com>
2023-11-07 03:19:42 +0100

    Basic REST Authentication class

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """
-6. Basic Auth
+6. Simulated Basic Authentication
 """
 from auth import Auth
 
 
 class BasicAuth(Auth):
     """Simulates Basic REST API Authentication"""
-    def __init__(self) -> None:
-        # super().__init__()
-        pass
+    # def __init__(self) -> None:
+    # super().__init__()
+    pass

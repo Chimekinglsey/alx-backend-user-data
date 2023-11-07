354378552ffb6d632d18442ec3b53f2541bec41d
chimekinglsey <onlinekingsley@gmail.com>
2023-11-07 04:09:49 +0100

    basic base64 authorization header encoding

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,8 +2,21 @@
 """
 6. Simulated Basic Authentication
 """
+import base64
 from .auth import Auth
 
 
 class BasicAuth(Auth):
     """Simulates Basic REST API Authentication"""
+    def __init__(self) -> None:
+        super().__init__()
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Returns Base64 encoded Authorization header """
+        if not authorization_header or not (isinstance
+                                            (authorization_header, str)):
+            return None
+        elif not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header.split()[1]

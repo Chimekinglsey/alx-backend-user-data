5bde9944d102e0e847314a7674840f8f6dea4eaa
chimekinglsey <onlinekingsley@gmail.com>
2023-11-07 20:39:35 +0100

    Handle '*' for excluded paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,18 +13,21 @@ class Auth:
         """Define which routes don't need authentication"""
         if path is None or excluded_paths is None or len(excluded_paths) < 1:
             return True
-        if path.endswith('*'):
-            path = path.rstrip('*')
+
         if not path.endswith('/'):
             path = path + '/'
+
         for route in excluded_paths:
-            if route.endswith('*'):
-                route = route.rstrip('*')
             if not route.endswith('/'):
                 route = route + '/'
+            if route.endswith('*'):
+                route = route.rstrip('*')
                 if path.startswith(route):
                     return True
-        return False if path in excluded_paths else True
+            else:
+                if path == route:
+                    return True
+        return path in excluded_paths
 
     def authorization_header(self, request=None) -> str:
         """Request validation"""

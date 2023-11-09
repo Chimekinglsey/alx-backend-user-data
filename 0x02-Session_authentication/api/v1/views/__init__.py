f2627c49a09eda0cdd7b2a2d8decac89974ff24b
chimekinglsey <onlinekingsley@gmail.com>
2023-11-10 00:51:16 +0100

    New view for session auth

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -7,5 +7,8 @@ app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
 from api.v1.views.users import *
+from api.v1.auth.session_auth import authenticate
 
 User.load_from_file()
+app_views.route('/auth_session/login',
+                methods=['POST'], strict_slashes=False)(authenticate)

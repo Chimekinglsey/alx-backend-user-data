b576f2a82e59f4907b69b655477815a823b4d9f8
chimekinglsey <onlinekingsley@gmail.com>
2023-11-09 15:18:48 +0100

    updated require auth method

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,6 +2,7 @@
 """Module for User Authentication Management"""
 from flask import request
 from typing import List, TypeVar
+import re
 
 
 class Auth:
@@ -11,32 +12,42 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Define which routes don't need authentication"""
-        if path is None or excluded_paths is None or len(excluded_paths) < 1:
-            return True
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = f'{exclusion_path[0:-1]}.*'
+                elif exclusion_path[-1] == '/':
+                    pattern = '{exclusion_path[0:-1]}/*'
+                else:
+                    pattern = f'{exclusion_path}/*'
+                if re.match(pattern, path):
+                    return False
+        return True
+        # if path is None or excluded_paths is None or len(excluded_paths) < 1:
+        #     return True
 
-        if not path.endswith('/'):
-            path = path + '/'
+        # if not path.endswith('/'):
+        #     path = path + '/'
 
-        for route in excluded_paths:
-            if not route.endswith('/'):
-                route = route + '/'
-            if route.endswith('*'):
-                route = route.rstrip('*')
-                if path.startswith(route):
-                    return True
-            else:
-                if path == route:
-                    return True
-        return False
+        # for route in excluded_paths:
+        #     if not route.endswith('/'):
+        #         route = route + '/'
+        #     if route.endswith('*'):
+        #         route = route.rstrip('*')
+        #         if path.startswith(route):
+        #             return True
+        #     else:
+        #         if path == route:
+        #             return True
+        # return path in excluded_paths
 
     def authorization_header(self, request=None) -> str:
         """Request validation"""
-        if request is None:
-            return None
-        elif not (request.headers.get('Authorization')):
-            return None
+        if request is not None:
+            return (request.headers.get('Authorization', None))
         else:
-            return request.headers.get('Authorization')
+            return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Returns None, will authenticate current user"""

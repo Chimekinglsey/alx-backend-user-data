4703ac1485bb56243b47f4bb64055541551abbb3
chimekinglsey <onlinekingsley@gmail.com>
2023-11-09 17:02:33 +0100

    return a valid user when 'me' is passed

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -28,12 +28,12 @@ def b4_request():
     valid_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
                    '/api/v1/forbidden/']
     if auth:
+        request.current_user = auth.current_user(request)
         if auth.require_auth(request.path, valid_paths):
             if auth.authorization_header(request) is None:
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
-
 
 @app.errorhandler(404)
 def not_found(error) -> str:

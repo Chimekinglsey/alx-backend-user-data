2dcdafe0bcfbe8d7072e144c5f2d4fe78dd26904
chimekinglsey <onlinekingsley@gmail.com>
2023-11-09 18:48:32 +0100

    Allow session-authenticated login

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -34,12 +34,11 @@ def b4_request():
     if auth:
         request.current_user = auth.current_user(request)
         if auth.require_auth(request.path, valid_paths):
-            if auth.authorization_header(request) is None:
+            if auth.authorization_header(request) is None and\
+                  auth.session_cookie(request) is None:
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
-    if auth.authorization_header(request) and auth.session_cookie(request):
-        return None, abort(401)
 
 
 @app.errorhandler(404)

79aa80bbac9ff5286653afc6f1f9c9ef5d61b22e
chimekinglsey <onlinekingsley@gmail.com>
2023-11-09 17:44:52 +0100

    create session_id for a user_id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,18 @@
 """
 Module for Session Authentication
 """
+from uuid import uuid4
 from .auth import Auth
 
 
 class SessionAuth(Auth):
     """Creates new authentication mechanism"""
+    user_id_by_session_id = dict()
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a Session ID for a user_id"""
+        if user_id and isinstance(user_id, str):
+            session_id = str(uuid4())
+            self.user_id_by_session_id['session_id'] = user_id
+            return session_id
+        return None

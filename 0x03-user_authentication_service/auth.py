d679869f7be2cc07bd7339c2bdf4789839e7c1fe
chimekinglsey <onlinekingsley@gmail.com>
2023-11-17 00:48:39 +0100

    Update reset link

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -75,6 +75,7 @@ class Auth:
         """Send reset password token to email"""
         user = self._db.find_user_by(email=email)
         if user:
-            user.reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=_generate_uuid())
+            # user.reset_token = _generate_uuid()
             return user.reset_token
         return ValueError

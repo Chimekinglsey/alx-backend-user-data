568d26f6cb73d0efeb7b5834f1c5bd45017f2b6f
chimekinglsey <onlinekingsley@gmail.com>
2023-11-16 21:55:46 +0100

    Bcrypt password hash

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,8 +1,18 @@
-#!usr/bin/env python3
-""" User Authentication Module"""
+#!/usr/bin/env python3
+""" User Authentication Module
+"""
 import bcrypt
+from db import DB
 
 
 def _hash_password(password: str) -> bytes:
     """Encrypt password"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()

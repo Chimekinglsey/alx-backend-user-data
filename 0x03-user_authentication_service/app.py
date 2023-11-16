b61e3d8199d46e0f440d832f32e02ec7f9f6a0fe
chimekinglsey <onlinekingsley@gmail.com>
2023-11-17 00:59:32 +0100

    Reset Password token view

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -63,5 +63,16 @@ def profile():
     abort(403)
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token():
+    """Send reset password token to email"""
+    email = request.form.get('email')
+    user = AUTH._db.find_user_by(email=email)
+    if user:
+        token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": user.email, "reset_token": token}), 200
+    abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", debug=True)

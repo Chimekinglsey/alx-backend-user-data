0c076a16a596f2a0ca43fb408fd2dad63d9c068e
chimekinglsey <onlinekingsley@gmail.com>
2023-11-16 23:14:45 +0100

    Login validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -24,3 +24,16 @@ class Auth:
                 raise ValueError("User {} already exists".format(email))
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: bytes):
+        """validate user credential for login"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                return bcrypt.checkpw(password.encode('utf-8'),
+                                      user.hashed_password)
+            return False
+        except NoResultFound:
+            return False
+        except Exception:
+            return False

34144940ecb0bb2f6575ff10db45870555ffd4a2
chimekinglsey <onlinekingsley@gmail.com>
2023-11-16 23:00:55 +0100

    Register users route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 """Basic Flask App"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from sqlalchemy.orm.exc import NoResultFound
+from auth import Auth
 
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -10,6 +13,19 @@ app = Flask(__name__)
 def index():
     """Welcome page"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """Create a new user"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        if AUTH._db.find_user_by(email=email):
+            return jsonify({"message": "email already registered"}), 400
+    except NoResultFound:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
 
 
 if __name__ == "__main__":

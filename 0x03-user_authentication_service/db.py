4f1db7df3dcbc3e52b9237059821430b6fe8603c
chimekinglsey <onlinekingsley@gmail.com>
2023-11-16 21:52:04 +0100

    bcrypt password hash

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -7,8 +7,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
-from typing import Dict, Any
-
+import bcrypt
 from user import Base, User
 
 

38b37b31ed812087d67419c663c6d3dd8f0cda36
chimekinglsey <onlinekingsley@gmail.com>
2023-11-17 02:40:21 +0100

    abort(403)

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -87,7 +87,7 @@ def update_password() -> str:
         AUTH.update_password(reset_token, new_password)
         passwd_reset_status = True
     except ValueError:
-       passwd_reset_status = False
+        abort(403)
     if passwd_reset_status is not True:
         abort(403)
     return jsonify({"email": email, "message": "Password updated"})
